33bc3fc58acea67ee375d5872015ece721c2f327
S Kumar <shiv.rv007@gmail.com>
2024-08-17 17:00:26 +0530

    Shivkumar version 3

---
+++ flaskproject/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+#added the new changes
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+
+def form():
+
+    if (request.method == 'POST'):   #POST
+
+        n = request.form.get('name')
+
+        a = request.form.get('age')
+
+        return render_template('index.html', n = n, a =a)
+
+    return render_template ('index.html')
+
+if __name__ == '__main__':
+
+    app.run(debug=True) #True (chnages for GIT)
+
+    #### modified for codefix branch ###########
